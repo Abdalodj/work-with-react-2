18a9cce50bca984317cf58f9ec62939e03379c5c
abdalodj <lodianea98@gmail.com>
2021-10-25 10:09:59 +0000

    Lesson P1C3

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Error from './components/Error';
+import Header from './components/Header';
 import './index.css';
 import Home from './pages/Home/index';
+import Survey from './pages/Survey';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/survey/:questionNumber'>
+          <Survey />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );

19c2d3ed116f74a3d6a853a05991daadd2748156
abdalodj <lodianea98@gmail.com>
2021-10-28 16:25:36 +0000

    Exercise P1C5 & I have re-do again for the sake of practicing css

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,26 +1,38 @@
-import propTypes from 'prop-types';
-import defaultPic from '../../assets/profile.png';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import DefaultPicture from '../../assets/profile.png';
 import colors from '../../utils/style/color';
 
 const CardLabel = styled.span`
   color: #5843e4;
   font-size: 22px;
-  font-weight: bold;
+  font-weight: normal;
+  padding-left: 15px;
 `;
+
+const CardTitle = styled.span`
+  color: black;
+  font-size: 22px;
+  font-weight: normal;
+  align-self: center;
+`;
+
 const CardImage = styled.img`
-  height: 80px;
-  width: 80px;
+  height: 150px;
+  width: 150px;
+  align-self: center;
   border-radius: 50%;
 `;
 
 const CardWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: space-around;
   padding: 15px;
   background-color: ${colors.backgroundLight};
   border-radius: 30px;
-  width: 350px;
+  width: 300px;
+  height: 300px;
   transition: 200ms;
   &:hover {
     cursor: pointer;
@@ -33,20 +45,21 @@ function Card({ label, title, picture }) {
     <CardWrapper>
       <CardLabel>{label}</CardLabel>
       <CardImage src={picture} alt='freelance' />
-      <span>{title}</span>
+      <CardTitle>{title}</CardTitle>
     </CardWrapper>
   );
 }
 
 Card.propTypes = {
-  label: propTypes.string.isRequired,
-  title: propTypes.string.isRequired,
-  picture: propTypes.string.isRequired
+  label: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired
 };
 
 Card.defaultProps = {
-  title: '',
   label: '',
-  picture: defaultPic
+  title: '',
+  picture: DefaultPicture
 };
+
 export default Card;
--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,41 +1,58 @@
-import styled from 'styled-components';
-import DefaultPicture from '../../assets/profile.png';
 import Card from '../../components/Card';
+import styled from 'styled-components';
+import colors from '../../utils/style/color';
 
 const CardsContainer = styled.div`
   display: grid;
-  gap: 2px;
+  gap: 24px;
   grid-template-rows: 350px 350px;
   grid-template-columns: repeat(2, 1fr);
+  align-items: center;
+  justify-items: center;
 `;
+
+const PageTitle = styled.h1`
+  font-size: 30px;
+  color: black;
+  text-align: center;
+  padding-bottom: 30px;
+`;
+
+const PageSubtitle = styled.h2`
+  font-size: 20px;
+  color: ${colors.secondary};
+  font-weight: 300;
+  text-align: center;
+  padding-bottom: 30px;
+`;
+
 const freelanceProfiles = [
   {
     name: 'Jane Doe',
-    jobTitle: 'Devops',
-    picture: DefaultPicture
+    jobTitle: 'Devops'
   },
   {
     name: 'John Doe',
-    jobTitle: 'Developpeur frontend',
-    picture: DefaultPicture
+    jobTitle: 'Developpeur frontend'
   },
   {
     name: 'Jeanne Biche',
-    jobTitle: 'Développeuse Fullstack',
-    picture: DefaultPicture
+    jobTitle: 'Développeuse Fullstack'
   }
 ];
 
 function Freelances() {
   return (
     <div>
-      <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
+      <PageTitle>Trouvez votre prestataire</PageTitle>
+      <PageSubtitle>
+        Chez Shiny nous réunissons les meilleurs profils pour vous.
+      </PageSubtitle>
       <CardsContainer>
         {freelanceProfiles.map((profile, index) => (
           <Card
             key={`${profile.name}-${index}`}
             label={profile.jobTitle}
-            picture={profile.picture}
             title={profile.name}
           />
         ))}

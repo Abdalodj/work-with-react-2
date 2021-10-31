82133b5523f3e68736b7f0600a4dda0e5923ac07
abdalodj <lodianea98@gmail.com>
2021-10-31 11:21:08 +0000

    Lesson bis P3C3

--- src/pages/Freelances/index.test.js
+++ src/pages/Freelances/index.test.js
@@ -1,50 +1,38 @@
 import { rest } from 'msw';
+import '@testing-library/jest-dom/extend-expect';
 import { setupServer } from 'msw/node';
-import {
-  waitFor,
-  screen,
-  waitForElementToBeRemoved
-} from '@testing-library/react';
-
-import Freelances from './';
+import { waitForElementToBeRemoved, screen } from '@testing-library/react';
 import { render } from '../../utils/test';
+import Freelances from './';
 
-describe('The Freelancers component', () => {
-  const freelancersMockedData = [
-    {
-      name: 'Harry Potter',
-      job: 'Magicien frontend',
-      picture: ''
-    },
-    {
-      name: 'Hermione Granger',
-      job: 'Magicienne fullstack',
-      picture: ''
-    }
-  ];
+const freelancersMockedData = [
+  {
+    name: 'Harry Potter',
+    job: 'Magicien frontend',
+    picture: ''
+  },
+  {
+    name: 'Hermione Granger',
+    job: 'Magicienne fullstack',
+    picture: ''
+  }
+];
 
-  const server = setupServer(
-    // On précise ici l'url qu'il faudra "intercepter"
-    rest.get('http://localhost:8000/freelances', (req, res, ctx) => {
-      // Là on va pouvoir passer les datas mockées dans ce qui est retourné en json
-      return res(ctx.json({ freelancersList: freelancersMockedData }));
-    })
-  );
+const server = setupServer(
+  rest.get('http://localhost:8000/freelances', (req, res, ctx) => {
+    return res(ctx.json({ freelancersList: freelancersMockedData }));
+  })
+);
 
-  // Active la simulation d'API avant les tests depuis server
-  beforeAll(() => server.listen());
-  // Réinitialise tout ce qu'on aurait pu ajouter en termes de durée pour nos tests avant chaque test
-  beforeEach(() => server.resetHandlers());
-  // Ferme la simulation d'API une fois que les tests sont finis
-  afterAll(() => server.close());
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
-  test('Should render without crash', async () => {
-    render(<Freelances />);
-    expect(screen.getByTestId('loader')).toBeTruthy();
-    await waitForElementToBeRemoved(() => screen.getByTestId('loader'));
-    await waitFor(() => {
-      expect(screen.getByText('Harry Potter')).toBeTruthy();
-      expect(screen.getByText('Hermione Granger')).toBeTruthy();
-    });
-  });
+it('Should display freelancers names after loader is removed', async () => {
+  render(<Freelances />);
+
+  await waitForElementToBeRemoved(() => screen.getByTestId('loader'));
+  expect(screen.getByText('Harry Potter')).toBeInTheDocument();
+  expect(screen.getByText('Hermione Granger')).toBeInTheDocument();
+  expect(screen.queryByTestId('loader')).not.toBeInTheDocument();
 });
